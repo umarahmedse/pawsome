9df1c4dcd22a059ca71d58fbd4c910e32a547ae0
Umar Ahmed <umarahmedse@outlook.com>
2023-12-15 06:59:59 +0500

    added cloudinary api config

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ import dotenv from "dotenv";
 import http from "http"; // Import http module
 import app from "../server/index.js";
 dotenv.config();
+import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
