51f00b49805cea1a88523407bdbaf0ec5b095223
Umar Ahmed <umarahmedse@outlook.com>
2023-12-15 07:00:18 +0500

    created addpet controller

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -1,13 +1,21 @@
 import petModel from "../models/petModel.js";
 import AppError from "../utils/appError.js";
 import catchAsync from "../utils/catchAsync.js";
-
+import { v2 as cloudinary } from "cloudinary";
 export const getAllPets = catchAsync(async (req, res, next) => {
-  console.log("first");
   const petsData = await petModel.find();
   if (!petsData) {
     return next(new AppError("No pets exist in database", 401));
   }
+  res.status(200).json({
+    status: "success",
+    petsData,
+  });
+});
+
+export const addPet = catchAsync(async (req, res, next) => {
+  const bodyData = req.body;
+  const petsData = await petModel.create(bodyData);
   res.status(200).json({
     status: "success",
     petsData,
